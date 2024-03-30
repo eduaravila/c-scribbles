d3b7febfcecdcd89a95f80b91ce832deefc87a2e
Eduardo Avila <github@eduaravila.com>
2024-03-29 21:05:21 -0600

    refactor wc clone

--- chap1/wc.c
+++ chap1/wc.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// example
 // word, word
 // w, wwww
-// nc = 15, nl=2, nw=4
+// nc = 21, nl=2, nw=4
+
+#define OUT 1
+#define IN 0
 
 // nc, nl, nw
+// '\n' also count is added in the char count
 int main(void)
 {
 
-    int input, inWord, nc, nl, nw;
-    input = inWord = nc = nl = nw = 0;
+    int input, notWord, nc, nl, nw;
+    input = notWord = nc = nl = nw = 0;
 
     while ((input = getchar()) != EOF)
     {
         // add line
-        if (input != ' ' && input != '\n')
-        {
-            inWord = 1;
-        }
-        if ((input == ' ' || input == '\n') && inWord == 1)
-        {
-            inWord = 0;
-            nw++;
-        }
         if (input == '\n')
         {
             nl++;
-            continue;
         }
+        if (input == ' ' || input == '\n' || input == '\t')
+        {
+            notWord = OUT;
+        }
+        else if (notWord == OUT){
+            notWord = IN;
+            nw++;
+        }
+
         nc++;
     }
 
