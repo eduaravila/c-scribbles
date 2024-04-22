9419086d6d70714fd33cf9c112d4124ef974ca8c
Eduardo Avila <github@eduaravila.com>
2024-04-21 22:05:32 -0600

    c: conver far to cel to func

--- chap1/fartocel.c
+++ chap1/fartocel.c
@@ -5,10 +5,16 @@
 #define UPER 300 /* lower limit of table */
 #define STEP 20  /* step size */
 
+float fartocel(float far);
+
 int main(void){
 
 for(int far = 0; far < UPER; far += STEP){
-        float cel =( ( far - 32 ) * ( 5.0 / 9.0 ) );
+        float cel = fartocel(far);
         printf("%6d\t%6.1f\n ", far, cel);
     };
+}
+
+float fartocel(float far){
+    return ( ( far - 32 ) * ( 5.0 / 9.0 ) );
 }
